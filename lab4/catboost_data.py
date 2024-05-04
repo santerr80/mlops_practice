476f88e701ae41c6bc7a05bc2c6f67ab195ba233
santerr80 <santerr80@mail.ru>
2024-05-04 19:51:05 +0700

    Add file catboost for get data

--- lab4/catboost_data.py
+++ lab4/catboost_data.py
@@ -1,1 +1,11 @@
-import catboost as cb
+from catboost.datasets import titanic
+import pandas as pd
+
+# загружаем датасет
+titanic_train, titanic_test = titanic()
+
+# сохраняем датасет в csv файл
+titanic_test.to_csv(r"C:\Users\Santerr80\OneDrive\Документы\GitHub\mlops_practice\lab4\datasets\titanic.csv", index=False)
+
+# выведем первые 3 строки датасета
+print(titanic_train.info())
