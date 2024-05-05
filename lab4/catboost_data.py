f522815c81fc79a06150d7374db36347410d4971
Александр Горбунов <santerr80@mail.ru>
2024-05-05 17:57:35 +0700

    Update datasets in class - Pclass Sex and Age

--- lab4/catboost_data.py
+++ lab4/catboost_data.py
@@ -1,11 +1,42 @@
 from catboost.datasets import titanic
 import pandas as pd
+import os
 
 # загружаем датасет
 titanic_train, titanic_test = titanic()
 
-# сохраняем датасет в csv файл
-titanic_test.to_csv(r"C:\Users\Santerr80\OneDrive\Документы\GitHub\mlops_practice\lab4\datasets\titanic.csv", index=False)
+pd.set_option('display.max_columns', None)
 
-# выведем первые 3 строки датасета
-print(titanic_train.info())
+# сохраняем датасет в csv файл
+try:
+    if os.path.exists(r"C:\Users\Santerr80\OneDrive\Документы\GitHub\
+                     mlops_practice\lab4\datasets\titanic.csv") is True:
+        pass
+    else:
+        titanic_test.to_csv(r"C:\Users\Santerr80\OneDrive\Документы\GitHub\
+                            mlops_practice\lab4\datasets\titanic.csv",
+                            index=False)
+except Exception as e:
+    print(e)
+
+# выведем первые строки датасета
+print(titanic_test.head())
+
+# внесем изменения в датасет
+titanic_test.iloc[0, 3] = 'female'
+titanic_test.iloc[0, 4] = 52.0
+
+# выведем первые строки датасета
+print(titanic_test.head())
+
+# сохраняем датасет в csv файл
+titanic_test.to_csv(r"C:\Users\Santerr80\OneDrive\Документы\GitHub\
+                    mlops_practice\lab4\datasets\titanic.csv", index=False)
+
+
+
+
+
+
+
+
